c6d98b04fd1d10ece972cf5dab3edd2ae5be069f
Tymoteusz Tula <tt343@icarus.kent.ac.uk>
2022-03-31 15:19:45 +0100

    runicarus

--- run_icarus.sh
+++ run_icarus.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=RandomSamplesCorrConn
-#SBATCH --output=/home/tt343/CorrelationConnection/run/output/icarus/outputs/array_%A_%a.out
-#SBATCH --error=/home/tt343/CorrelationConnection/run/output/icarus/errors/array_%A_%a.err
-#SBATCH --array=1-2
+#SBATCH --output=/home/tt343/CorrelationConnection/run/output/icarus/outputs/run31032022/array_%A_%a.out
+#SBATCH --error=/home/tt343/CorrelationConnection/run/output/icarus/errors/run31032022/array_%A_%a.err
+#SBATCH --array=1-90%4
 #SBATCH --mail-type=END
 #SBATCH --mail-user=tt343@kent.ac.uk 
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1000
 #SBATCH --partition=cpu
 
-echo "hello world"
-
 echo "Starting job $SBATCH_JOB_NAME $SLURM_JOB_ID task ${SLURM_ARRAY_TASK_ID} running on $SLURM_JOB_NODELIST"
-python /home/tt343/CorrelationConnection/run/run_on_icarus.py ${SLURM_ARRAY_TASK_ID}
+python /home/tt343/CorrelationConnection/run/run_on_icarus.py ${SLURM_ARRAY_TASK_ID} --input_folder run31032022/
 echo "Finished job $SLURM_JOB_ID on $HOSTNAME node list $SLURM_JOB_NODELIST"
