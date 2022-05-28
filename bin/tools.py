46800d9bb579eb1d3f436764d277a71b4cb72b48
TymoteuszTula <tymoteusz.tula@gmail.com>
2022-05-28 20:44:58 +0100

    Removed bug

--- bin/tools.py
+++ bin/tools.py
@@ -605,10 +605,7 @@ class SijCalculator:
 
         
     def find_gs_sparse(ham):
-        #return sprsla.eigsh(ham, 1, which="SA")
-        ham_den = ham.todense()
-        evs = np.linalg.eigvals(ham_den)
-        return [np.min(evs)], 0
+        return sprsla.eigsh(ham, 1, which="SA")
 
     def find_eigvals(ham):
         return np.linalg.eigvals(ham)
